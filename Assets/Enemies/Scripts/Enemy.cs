81e1634bd1257a9937d44ac846a8f70192ae9335
Unknown <epiceditiongames@gmail.com>
2018-07-06 16:40:55 -0500

    Section 2 Lecture 67

---
+++ Assets/Enemies/Scripts/Enemy.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(AICharacterControl))]
+public class Enemy : MonoBehaviour, IDamageable {
+
+    [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float chaseRadius = 8f;
+    [SerializeField] float attackRadius = 3f;
+    [SerializeField] float damagePerShot = 9f;
+    [SerializeField] float secondsBetweenShots = 0.5f;
+    [SerializeField] GameObject projectileToUse;
+    [SerializeField] GameObject projectileSocket;
+    [SerializeField] Vector3 aimOffset = new Vector3(0, 1, 0);
+
+    bool isAttacking = false;
+    float currentHealthPoints = 100f;
+    AICharacterControl aiCharacterControl;
+    Transform playerTransform;
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    void Start()
+    {
+        aiCharacterControl = GetComponent<AICharacterControl>();
+        var playerGO = GameObject.FindGameObjectWithTag("Player");
+        if (playerGO != null)
+        {
+            playerTransform = playerGO.transform;
+        }
+        else
+        {
+            Debug.LogWarning("Couldn't find player tagged \"Player\"!");
+        }
+        currentHealthPoints = maxHealthPoints;
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+        if (distanceToPlayer <= attackRadius && !isAttacking)
+        {
+            isAttacking = true;
+            InvokeRepeating("SpawnProjectile", 0f, secondsBetweenShots); // TODO switch to coroutines
+        }
+        if (distanceToPlayer > attackRadius)
+        {
+            isAttacking = false;
+            CancelInvoke();
+        }
+
+        if (distanceToPlayer <= chaseRadius)
+        {
+            aiCharacterControl.SetTarget(playerTransform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
+        }
+    }
+
+    void SpawnProjectile()
+    {
+        var projectileGO = Instantiate(projectileToUse, projectileSocket.transform.position, Quaternion.identity);
+        var projectile = projectileGO.GetComponent<Projectile>();
+        projectile.SetDamage(damagePerShot);
+
+        Vector3 unitVector = (playerTransform.position + aimOffset - projectileSocket.transform.position).normalized;
+        projectileGO.GetComponent<Rigidbody>().velocity = unitVector * projectile.projectileSpeed;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
+        if (currentHealthPoints <= 0) { Destroy(gameObject); }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(255f, 0, 0.5f);
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+        Gizmos.color = new Color(0, 0, 255f, .5f);
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
+    }
+}

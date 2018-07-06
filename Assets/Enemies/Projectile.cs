ef1a69eff477d169bd823661ad9225190753c854
Unknown <epiceditiongames@gmail.com>
2018-07-06 14:01:38 -0500

    First

---
+++ Assets/Enemies/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float projectileSpeed;
+
+    float damageCaused;
+
+    public void SetDamage(float damage)
+    {
+        damageCaused = damage;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        var damageable = col.GetComponent(typeof(IDamageable));
+        if (damageable != null)
+        {
+            (damageable as IDamageable).TakeDamage(damageCaused);
+        }
+    }
+
+}
